e069a0e21b0d0bfd6c2ed7ef2bdbe8f6c294f9f7
alex <alexinfopruna@gmail.com>
2017-04-27 09:31:04 +0200

    alergogens

--- wp-content/themes/onetone_cb/js/alergogens.js
+++ wp-content/themes/onetone_cb/js/alergogens.js
@@ -5,6 +5,8 @@
  */
 
 jQuery(document).ready(function ($) {
+    
+if (lang=="ca"){    
  $(".alergogens-alt").attr('title','Tramussos');
  $(".alergogens-api").attr('title','Api');
  $(".alergogens-cac").attr('title','Cacauet');
@@ -19,4 +21,44 @@ jQuery(document).ready(function ($) {
  $(".alergogens-mos").attr('title','mostassa');
  $(".alergogens-pes").attr('title','Peix');
  $(".alergogens-soj").attr('title','Soja');
+ }
+ 
+ 
+ if (lang=="ca"){    
+ $(".alergogens-alt").attr('title','Altramuces');
+ $(".alergogens-api").attr('title','Apio');
+ $(".alergogens-cac").attr('title','Cacahuete');
+ $(".alergogens-glu").attr('title','Glúten');
+ $(".alergogens-cru").attr('title','Crustaceos');
+ $(".alergogens-dio").attr('title','Dióxido de azufre y sulfitos');
+ $(".alergogens-fru").attr('title','Frutos con cáscara');
+ $(".alergogens-gra").attr('title','Granos de sésamo');
+ $(".alergogens-hue").attr('title','Huevoss');
+ $(".alergogens-lac").attr('title','Lácteos');
+ $(".alergogens-mol").attr('title','Moluscos');
+ $(".alergogens-mos").attr('title','mostaza');
+ $(".alergogens-pes").attr('title','Pescado');
+ $(".alergogens-soj").attr('title','Soja');
+ }
+ 
+ 
+ 
+ if (lang=="en"){    
+ $(".alergogens-alt").attr('title','Lupin');
+ $(".alergogens-api").attr('title','Celery');
+ $(".alergogens-cac").attr('title','Peanuts');
+ $(".alergogens-glu").attr('title','Glúten');
+ $(".alergogens-cru").attr('title','Crustaceans');
+ $(".alergogens-dio").attr('title','Sulphur dioxide');
+ $(".alergogens-fru").attr('title','Nuts');
+ $(".alergogens-gra").attr('title','Sesame seeds');
+ $(".alergogens-hue").attr('title','Eggs');
+ $(".alergogens-lac").attr('title','Milk');
+ $(".alergogens-mol").attr('title','Molluscs');
+ $(".alergogens-mos").attr('title','Mustard');
+ $(".alergogens-pes").attr('title','Fish');
+ $(".alergogens-soj").attr('title','Soya');
+ }
+ 
+ 
 })
