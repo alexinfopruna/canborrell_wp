0e9ed4b533a6477df281cf28c495208cba14db5e
icastelletdam2 <alexinfopruna@gmail.com>
2017-11-09 07:07:35 +0100

    youtube

--- youtube.php
+++ youtube.php
@@ -18,12 +18,9 @@
           
       </style>
       
-      <div class="videoWrapper">
+      <div class="videoWrapper"><div id="player"></div></div>
     <!-- Copy & Pasted from YouTube -->
-      
     <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
-    <div id="player"></div>
-</div>
     <script>
       // 2. This code loads the IFrame Player API code asynchronously.
       var tag = document.createElement('script');
@@ -41,7 +38,7 @@
           width: '640',
           videoId: 'M7lc1UVf-VE',
            playerVars: { 
-         'autoplay': 0,
+         'autoplay': 1,
          'controls': 0, 
          'rel' : 0,
          'fs' : 0,
@@ -53,14 +50,9 @@
         });
       }
 
-      // 4. The API will call this function when the video player is ready.
       function onPlayerReady(event) {
-        event.target.playVideo();
       }
 
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
       var done = false;
       function onPlayerStateChange(event) {
         if (event.data == YT.PlayerState.PLAYING && !done) {
