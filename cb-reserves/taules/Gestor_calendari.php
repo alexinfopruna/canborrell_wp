e91403645ecc1bb09382bab4058824fb42b729da
icastelletdam2 <alexinfopruna@gmail.com>
2018-06-27 16:57:11 +0200

    dies especials falla

--- cb-reserves/taules/Gestor_calendari.php
+++ cb-reserves/taules/Gestor_calendari.php
@@ -126,12 +126,12 @@ class Gestor_calendari extends Gestor {
       $date=Gestor::cambiaf_a_mysql($_GET['data']);
       $date = "'$date'";
     }
-    echo "WWWWWWWW";die();
+    //echo "WWWWWWWW";die();
     if (!$date) $date="CURRENT_DATE";
     
     $query = "SELECT * FROM dies_especials_$group WHERE dies_especials_data <= $date + INTERVAL 360 DAY";
    
-   // echo $date." -- -".$query;
+   echo $date." -- -".$query;
     $this->qry_result = mysqli_query($this->connexioDB, $query) or die(((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
     if (!$this->total_rows = mysqli_num_rows($this->qry_result)) {
       return "[]";
