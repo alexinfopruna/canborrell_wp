d7f2931ec3154f7fcef3b8595fd37b106ff4e28a
icastelletdam2 <alexinfopruna@gmail.com>
2019-05-11 13:00:29 +0200

    ataula, relative right, bottom

--- cb-reserves/taules/gestor_reserves.php
+++ cb-reserves/taules/gestor_reserves.php
@@ -1243,7 +1243,7 @@ class gestor_reserves extends Gestor {
       $impagada = ( $row['estat'] != 100) ? "background:#EDFF00;" : "";
       $title = ( $row['estat'] != 100) ? 'title="Pendent de pagament"' : "";
 
-      $ataula = '<input title="Sín a taula" type="checkbox" style="float:right;position:relative;right:2px;bottom:16px;" id="switch-'.$row['id_reserva'].'" idr="'.$row['id_reserva'].'" class="chekataula"  '.$chekataula.'>';
+      $ataula = '<input title="Són a taula" type="checkbox" style="float:right;position:relative;right:2px;bottom:16px;" id="switch-'.$row['id_reserva'].'" idr="'.$row['id_reserva'].'" class="chekataula"  '.$chekataula.'>';
    //  $ataula = "";
       
       $data = "";
