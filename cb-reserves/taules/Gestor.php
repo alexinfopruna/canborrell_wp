51cedc7fbbe4d29a490882a9b0f8c640b062cf7a
Àlex Garcia <agarci9@xtec.cat>
2020-01-20 22:25:27 +0100

    tpv authentication PROVES

--- cb-reserves/taules/Gestor.php
+++ cb-reserves/taules/Gestor.php
@@ -943,7 +943,7 @@ class Gestor {
     $this->xgreg_log("generaFormTpvSHA256 $id_reserva $import $nom", 0, LOG_FILE_TPVPK, TRUE);
 //echo"$id_reserva s  $import s $nom s  $tpv_ok_callback_alter ssssssssss";
     //$client=$this->clientReserva($id_reserva);
-    if($_SESSION['permisos']>120 && isset($_GET["testAuth"])) $import=0.5;
+    if( isset($_GET["testAuth"])) $import=0.5;
     //$import=0.5;isset($_GET["testAuth"])
     $id = $order = substr(time(), -4, 3) . $id_reserva;
 
@@ -962,7 +962,7 @@ class Gestor {
     // Se incluye la librería
     include ROOT.INC_FILE_PATH . 'API_PHP/redsysHMAC256_API_PHP_5.2.0/apiRedsys.php';
    
-    if ( $_SESSION['permisos']>120 && isset($_GET["testAuth"]) && $tpv_ok_callback="reserva_pk_tpv_ok_callback") $trans=7;
+    if (isset($_GET["testAuth"]) && $tpv_ok_callback="reserva_pk_tpv_ok_callback") $trans=7;
     
     // Se crea Objeto
     $miObj = new RedsysAPI;
