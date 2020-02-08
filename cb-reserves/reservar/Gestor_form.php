c39d9291056fcc007006df9eea860dbb1f190a10
Àlex Garcia <agarci9@xtec.cat>
2020-02-08 07:53:00 +0100

    text sms

--- cb-reserves/reservar/Gestor_form.php
+++ cb-reserves/reservar/Gestor_form.php
@@ -1599,7 +1599,7 @@ WHERE  `client`.`client_id` =$idc;
     $args[] = $idr;
     $args[] = $import;
     $missatge = "Recordi: reserva al Restaurant Can Borrell. %s %s (%s).Preguem comuniqui qualsevol canvi: 936929723/936910605.Gràcies.(ID%s)";
-    $missatge .= "\n***\nConservi quest SMS com a comprovant de la paga y senyal de %s€ que li serà descomptada. També hem enviat un email que pot imprimir";
+    $missatge .= "\n***\nConservi aquest SMS com a comprovant de la reserva. També hem enviat un email que pot imprimir";
     $missatge = gestor_reserves::SMS_language($missatge, $row['lang'], $args);
     $this->enviaSMS($idr, $missatge);
 
