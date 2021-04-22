714d3561f20c7e3f1e4c52a36970dc5595a4d688
El vostre nom <usuari@domini.com>
2021-04-22 23:53:26 +0200

    grups submit - session...

--- cb-reserves/editar/reserves.php
+++ cb-reserves/editar/reserves.php
@@ -160,8 +160,7 @@ function mail_plantilla($id = false) {
   $avui = date("d/m/Y");
   $ara = date("H:i");
   /////// ATENCIO
-  echo getcwd();die();
-  $file = ROOT . "../editar/templates/pre_reserva_rest.lbi";
+  $file = getcwd(). "/templates/pre_reserva_rest.lbi";
 
 
   $t = new Template('.', 'comment');
