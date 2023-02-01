5ba1da512ca0e8afd60e2b0b88442acc33c2b581
alexinfopruna <alexinfopruna@gmail.com>
2023-02-01 14:06:58 +0100

    recordatoris grups- cadicades ON

--- cb-reserves/editar/caducades.php
+++ cb-reserves/editar/caducades.php
@@ -1,7 +1,7 @@
 <?php
 $root= getcwd()."/../taules/";
 if (!defined('ROOT'))  define('ROOT', $root);
-echo ROOT;die();
+//echo ROOT;die();
 require(ROOT . "gestor_reserves.php");
 $gestor = new gestor_reserves();
 require(ROOT . "Gestor_pagaments.php");
