71c560bfd0a4891d64675c4cd8b9a977a600a6ae
alexinfopruna <alexinfopruna@gmail.com>
2024-11-28 00:07:54 +0100

    amf

--- cb-reserves/taules/amfphp/Services/ControlTaules.php
+++ cb-reserves/taules/amfphp/Services/ControlTaules.php
@@ -1,6 +1,12 @@
 <?php
 define('AMF', true);
 define('ROOT', "../");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
+
 
 include("vo/com/canBorrell/EstatTaulesVO.php");
 include("vo/com/canBorrell/EstatMenjadorVO.php");
