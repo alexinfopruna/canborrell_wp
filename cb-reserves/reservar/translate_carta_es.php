26d1bc4f5805bcc166691845f90216145f3c2d34
alexinfopruna <alexinfopruna@gmail.com>
2025-01-17 23:41:40 +0100

    traduccions

--- cb-reserves/reservar/translate_carta_es.php
+++ cb-reserves/reservar/translate_carta_es.php
@@ -8,160 +8,198 @@ if (!isset($PERSONES_GRUP))
 // MENU
 
 
+/**/
+$translate['(PRIORAT) MAIUS ASSEMBLATGE'] = '(Priorat) Maius Ensamblaje';
+$translate['ALL I OLI ( Rció per Pers'] = 'All i oli';
+$translate['ALL I OLI ( Rció per Persona )'] = 'Alioli (Ración por Persona)';
+$translate['ALMENDRADO'] = 'Almendrado';
+$translate['Amanida amb Formatge Tendre'] = 'Ensalada con queso Feta';
 
-$translate['RACIONES'] = 'RACIONES';
-$translate['FORMATGE D´OVELLA'] = 'Queso de oveja';
-$translate['PERNIL SERRÁ'] = 'Jamón serrano';
-$translate['LLONGANISSA'] = 'Longaniza';
-$translate['XORIÇO'] = 'Chorizo';
-$translate['MENU GRUPO Nº 4'] = 'MENU GRUPO Nº 4 <i style="color:blue">No combinable: Cal que tots els comensals demanin el mateix</i>';
-$translate['HORARI_ENTREPANS'] = 'Los bocadillos sólo se sirven de 10:00h a 12:00h';
-$translate['Menu de calçotada(hasta 25 u aprox por comensal)'] = 'Menú de calçotada(hasta a 25u aprox)<i style="color:blue"> No combinable: Todos deben pedir lo mismo</i>';
-$translate['MENU DE CALÇOTADA(hasta 25 u aprox por comensal)'] = 'MENU DE CALÇOTADA(hasta a 25u aprox)<i style="color:blue"> No combinable: Todos deben pedir lo mismo</i>';
-
-
-
-$translate['ENTRANTES'] = 'Entrantes'; /*Dubte*/
-$translate['Amanida amb Formatge Tendre'] = 'Ensalada con queso Feta';/*Dubte*/
+$translate['Amanida amb Tomaquet,Ceva i Tonyina'] = 'Ensalada con Tomate, Cebolla y Atún';
 $translate['Amanida Catalana amb Embotits'] = 'Ensalada Catalana con Embutidos';
 $translate['Amanida Escarola Tebia amb Formatge de Cabra'] = 'Ensalada de escarola tibia con Queso de Cabra';
 $translate['AMANIDA VERDA'] = 'Ensalda verde';
+
 $translate['ANXOVES DE L´ESCALA'] = 'Anchoas de la Escala';
-$translate['Canalons Casolans (3 peçes)'] = 'Canalones Caseros (3 piezas)';
-$translate['CARGOLS A LA LLAUNA'] = 'Caracoles a la "llauna"';
-$translate['CARGOLS'] = 'Caracoles de la casa';
-$translate['CARXOFES A LA BRASA'] = 'Alcachofas a la brasa (temporada)';
-$translate['ESTREMES'] = 'Entremeses';
-$translate['ESCALIVADA'] = 'Escalivada';
-$translate['ESPARRECS BLADERS A LA BRASA'] = 'Espárragos trigueros a la brasa';
-$translate['Esparrecs Blancs amb Mayonesa'] = 'Espárragos Blancos con Mayonesa';
-$translate['ESQUEIXADA'] = 'Esqueixada';
-$translate['GIRGOLES A LA BRASA'] = 'Setas a la brasa';
-$translate['PARRILLADA DE VERDURES A LA BRASA'] = 'parrillada de verduras a la brasa';
-$translate['MACARRONS'] = 'Macarrones de la casa';
-$translate['MELÓ AMB PERNIL'] = 'Melón con jamón';
-$translate['Pernil Ibèric d´glá'] = 'Jamón Ibérico de bellota';
-$translate['PINYA AMB PERNIL'] = 'Piña con jamón';
-$translate['CALÇOTS (TEMPORADA)'] = 'Calçots (temporada)';
-$translate['SOPA DE LA CASA'] = 'Sopa de la casa';
-$translate['SURTIT DE PATES'] = 'Surtido de patés';
-$translate['TORRADA AMB ESCALIVADA I ANXOVES'] = 'Tostada con escalivada, anchoas' y atún;
-$translate['Xatonada casolana'] = 'Xatonada casera';
-$translate['CARNES'] = 'CARNES';
-$translate['BOTIFFARA A LA BRASA'] = 'Butifarra a la brasa';
-$translate['BOTIFARRA NEGRA A LA BRASA'] = 'Butifarra negra a la brasa';
-$translate['CONILL A LA BRASA'] = 'Conejo a la brasa (1/2 pieza)';
-$translate['COSTELLA DE VEDELLA A LA BRASA'] = 'Costilla de ternera a la brasa';
-$translate['COSTELLA DE XAI A LA BRASA'] = 'Costilla de cordero a la brasa';
-$translate['costellar de Porc Ibèric a la brasa (450gr)'] = 'costillar de Cerdo Ibérico a la brasa (450gr)';
-$translate['ESPATLLA DE XAI'] = 'Espalda de cordero';
-$translate['GALTA DE PORC A LA BRASA'] = 'Quijada de cerdo a la brasa';
-$translate['PEUS DE PORC A LA BRASA'] = 'Pies de cerdo a la brasa';
-$translate['POLLASTRE A LA BRASA'] = 'Pollo a la brasa (1/2 pieza)';
-$translate['Secret de Porc a la brasa ibèric (250gr)'] = 'Secreto de Cerdo a la brasa ibérico (250gr)';
-$translate['VEDELLA AMB VOLETS'] = 'Ternera con setas';
-$translate['XAI A LA BRASA (3 PECES)'] = 'Cordero a la brasa (3 piezas)';
-$translate['PESCADO'] = 'Pescado';
+
 $translate['Bacallà a la LLauna amb Muselina all i oli (brasa)'] = 'Bacalao a la "Llauna" con Muselina Alioli (brasa)';
-$translate['LLOBARRO A LA BRASA'] = 'Lubina a la brasa';
-$translate['MOIXARRA A LA BRASA'] = 'Dorada a la brasa';
-$translate['RACIONES'] = 'RACIONES';
-$translate['Amanida amb Tomaquet,Ceva i Tonyina'] = 'Ensalada con Tomate, Cebolla y Atún';
-$translate['FORMATGE D´OVELLA'] = 'Queso de oveja';
-$translate['Formatge d´ovella 1/2 Ració'] = 'Queso de oveja 1/2 Ración';
-$translate['LLONGANISSA'] = 'Longaniza';
-$translate['LLonganissa Cular 1/2 Ració'] = 'Longaniza Cular 1/2 Ración';
-$translate['Mongetas del Ganxet amb Cansalada 1/2 Ració'] = 'Judías del Ganxet con Tocino 1/2 Ración';
-$translate['Mongetes del Ganxet 1/2 Ració'] = 'Judías del Ganxet 1/2 Ración';
-$translate['Patates al Caliu 1/2 ració'] = 'Patatas al Caliu 1/2 ración';
-$translate['Patates Fregides 1/2 Ració'] = 'Patatas Fritas 1/2 Ración';
-$translate['Pernil Ibèric d´Gla 1/2 Ració'] = '-Jamón Ibérico de Bellota 1/2 Ración';
-$translate['PERNIL SERRÁ'] = 'Jamón serrano';
-$translate['Pernil Serrà GR (duroc) 1/2 Ració'] = 'Jamón Serrano GR (duro) 1/2 Ración';
-$translate['XORIÇO'] = 'Chorizo';
-$translate['Xoriço Cular 1/2 Ració'] = 'Chorizo ​​Cular 1/2 Ración';
-$translate['COMPLEMENTOS'] = 'COMPLEMENTOS';
-$translate['ALL I OLI ( Rció per Persona )'] = 'Alioli (Ración por Persona)';
-$translate['MONGETES'] = 'Judías';
-$translate['MONGETES AMB CANSALADA'] = 'Judías con tocino';
-$translate['PA TORRAT AMB TOMAQUET'] = 'Pan tostado con tomate';
-$translate['PA TORRAT'] = 'Pan tostado';
-$translate['Patates al Caliu'] = 'Patatas en el Caliu';
-$translate['PATATES FREGIDES'] = 'Patatas fritas';
 $translate['BOTELLAS'] = 'Vinos';
-$translate['Copa de Vi (Eternium-cabernet) Penedés'] = 'Copa de Vino (Eternium-cabernet) Penedés';
-$translate['Eternium (chardonnay barrica) Penedès Blanc'] = 'Eternium (chardonnay barrica) Penedès Blanco';
-$translate['Jose Pariente (verdejo) Rueda'] = 'Jose Pariente (verdejo) Rueda';
-$translate['(PRIORAT) MAIUS ASSEMBLATGE'] = '(Priorat) Maius Ensamblaje';
-$translate['Mar de Frades (alvariño) Rias Baixas'] = 'Mar de Frades (alvariño) Rias Baixas';
-$translate['Marques de Caceres Bot. 1/2 ( Rioja Negre)'] = 'Marqués de Cáceres Bot. 1/2 (Rioja Negro)';
-$translate['RIOJA MARQUES DE CACERES CRIANZA'] = 'Rioja marqués de Cáceres, crianza';
-$translate['RIOJA MARQUES DE RISCAL RESERVA'] = 'Rioja marqués de riscal reserva';
-$translate['Matarromera (valbuena de duero) Tempranillo'] = 'Matarromera (valbuena de duero) Tempranillo';
-$translate['PINNA FIDELIS RIVERA DUERO CRIANZA'] = 'Pinna fieles ribera Duero crianza';
-$translate['Raimat Abadia Cabernet Sauvignon Negre'] = 'Raimat Abadía Cabernet Sauvignon Negro';
-$translate['ramon bilbao 1/2 pequeño'] = 'ramon bilbao 1/2 pequeño';
-$translate['RIOJA MARQUES DE MURRIETA'] = 'marqués de Murrieta - Rioja';
-$translate['YLLERA - CASTELLA I LLEÓ-'] = 'Yllera - castilla y león';
-$translate['Roca Blanca (Montsant negre) Crianza'] = 'Roca Blanca (Montsant negre) Crianza';
-$translate['CAVAS'] = 'CAVAS';
+
+$translate['BOTIFARRA NEGRA A LA BRASA'] = 'Butifarra negra a la brasa';
+
+$translate['BOTIFFARA A LA BRASA'] = 'Butifarra a la brasa';
+$translate['BRUT RESERVA SARDÁ'] = 'Brut reserva sardá';
+$translate['CALÇOTS (12 UN APRX)'] = 'Calçots (temporada) ';
+$translate['CALÇOTS (TEMPORADA)'] = 'Calçots (temporada)';
+$translate['CANALONES'] = 'Canalones';
+$translate['Canalons Casolans (3 peçes)'] = 'Canalones Caseros (3 piezas)';
+$translate['CAPDELLS AMB TONYINA I AN'] = 'Cogollos con atún y anchoas';
+
+$translate['CARGOLS A LA LLAUNA'] = 'Caracoles a la "llauna"';
+
+$translate['CARGOLS'] = 'Caracoles de la casa';
+
+$translate['CARNES']='CARNES';
+$translate['CARXOFES A LA BRASA'] = 'Alcachofas a la brasa (temporada)';
+
 $translate['CAVA ANGEL CUPATGE'] = 'Cava Angel Cupatge';
 $translate['Cava Benjamin Codorniu'] = 'Cava Benjamin Codorniu';
 $translate['Cava Brut Reserva de la Casa'] = 'Cava Brut Reserva de la Casa';
-$translate['JUVE CAMPS BRUT NATURE'] = 'Juve camps brut nature'
-$translate['BRUT RESERVA SARDÁ'] = 'Brut reserva sardá';
-$translate['POSTRES'] = 'POSTRES';
+$translate['CAVAS'] = 'CAVAS';
+$translate['COCO HELADO'] = 'Coco helado';
+
+$translate['COMPLEMENTOS'] = 'COMPLEMENTOS';
+
+$translate['CONILL A LA BRASA'] = 'Conejo a la brasa (1/2 pieza)';
+$translate['Copa de Vi (Eternium-cabernet) Penedés'] = 'Copa de Vino (Eternium-cabernet) Penedés';
+$translate['CORNETTO VAINILLA Y CHOC'] = 'Corneto vainilla y chocolate';
+$translate['COSTELLA DE VEDELLA A LA '] = 'Chuletón de ternera a la brasa';
+$translate['COSTELLA DE VEDELLA A LA BRASA'] = 'Costilla de ternera a la brasa';
+$translate['COSTELLA DE XAI A LA BRAS'] = 'Costilla de cordero a la brasa (unidad)';
+$translate['COSTELLA DE XAI A LA BRASA'] = 'Costilla de cordero a la brasa';
+$translate['costellar de Porc Ibèric a la brasa (450gr)'] = 'costillar de Cerdo Ibérico a la brasa (450gr)';
 $translate['Crema Catalana amb Nata'] = 'Crema Catalana con Nata';
 $translate['Crema Catalana Casolana'] = 'Crema Catalana Casera';
-$translate['Flam Casolà'] = 'Flan Casero';
+$translate['CROCANTI CON COINTREAU'] = 'Crocanti con cointreau';
+$translate['CROCANTI CON NATA'] = 'Crocanti con nata';
+$translate['CROCANTI'] = 'Crocanti';
+$translate['ENTRANTES'] = 'Entrantes'; /*Dubte*/
+
+$translate['ESCALIVADA'] = 'Escalivada';
+$translate['Espárragos blancos'] = 'Espárragos blancos';
+$translate['ESPARRECS BLADERS A LA BR'] = 'Espárragos trigueros a la brasa';
+$translate['ESPARRECS BLADERS A LA BRASA'] = 'Espárragos trigueros a la brasa';
+$translate['Esparrecs Blancs amb Mayonesa'] = 'Espárragos Blancos con Mayonesa';
+
+$translate['ESPATLLA DE XAI'] = 'Espalda de cordero';
+
+$translate['ESQUEIXADA'] = 'Esqueixada';
+
+$translate['ESTREMES'] = 'Entremeses';
+$translate['Eternium (chardonnay barrica) Penedès Blanc'] = 'Eternium (chardonnay barrica) Penedès Blanco';
 $translate['Flam Casolà amb Nata'] = 'Flan Casero con Nata';
+$translate['Flam Casolà'] = 'Flan Casero';
+$translate['Formatge d´ovella 1/2 Ració'] = 'Queso de oveja 1/2 Ración';
+
+$translate['FORMATGE D´OVELLA'] = 'Queso de oveja';
 $translate['FRUTA NATURAL'] = 'Fruta natural';
+
+$translate['GALTA DE PORC A LA BRASA'] = 'Quijada de cerdo a la brasa';
+
+$translate['GIRGOLES A LA BRASA'] = 'Setas a la brasa';
+$translate['HELADO DE VAINILLA'] = 'Helado de vainilla';
+$translate['HORARI_ENTREPANS'] = 'Los bocadillos sólo se sirven de 10:00h a 12:00h';
+$translate['Jose Pariente (verdejo) Rueda'] = 'Jose Pariente (verdejo) Rueda';
+$translate['JUVE CAMPS BRUT NATURE'] = 'Juve camps brut nature'
+$translate['LIMON HELADO'] = 'Limon helado';
+
+$translate['LLOBARRO A LA BRASA'] = 'Lubina a la brasa';
+$translate['LLOM A LA BRASA'] = 'Lomo a la brasa';
+$translate['LLonganissa Cular 1/2 Ració'] = 'Longaniza Cular 1/2 Ración';
+
+$translate['LLONGANISSA'] = 'Longaniza';
+
+$translate['MACARRONS'] = 'Macarrones de la casa';
 $translate['Maduixots amb Moscatell'] = 'Fresas con Moscatel';
 $translate['Maduixots amb Nata'] = 'Fresones con nata';
 $translate['Maduixots amb Suc de Taronja'] = 'Fresones con Zumo de Naranja';
+$translate['Mar de Frades (alvariño) Rias Baixas'] = 'Mar de Frades (alvariño) Rias Baixas';
+$translate['Marques de Caceres Bot. 1/2 ( Rioja Negre)'] = 'Marqués de Cáceres Bot. 1/2 (Rioja Negro)';
+$translate['Matarromera (valbuena de duero) Tempranillo'] = 'Matarromera (valbuena de duero) Tempranillo';
 $translate['Mel i Mató Casolà'] = 'Miel y Mató Casero';
+
+
+$translate['MELÓ AMB PERNIL'] = 'Melón con jamón';
 $translate['Meló Fruita Natural'] = 'Melón Fruta Natural';
+$translate['MENU DE CALÇOTADA(hasta 25 u aprox por comensal)'] = 'MENU DE CALÇOTADA(hasta a 25u aprox)<i style="color:blue"> No combinable: Todos deben pedir lo mismo</i>';
+$translate['Menu de calçotada(hasta 25 u aprox por comensal)'] = 'Menú de calçotada(hasta a 25u aprox)<i style="color:blue"> No combinable: Todos deben pedir lo mismo</i>';
+$translate['MENU GRUPO Nº 4'] = 'MENU GRUPO Nº 4 <i style="color:blue">No combinable: Cal que tots els comensals demanin el mateix</i>';
+
+$translate['MOIXARRA A LA BRASA'] = 'Dorada a la brasa';
+$translate['Mongetas del Ganxet amb Cansalada 1/2 Ració'] = 'Judías del Ganxet con Tocino 1/2 Ración';
+
+$translate['MONGETES AMB CANSALADA'] = 'Judías con tocino';
+$translate['Mongetes del Ganxet 1/2 Ració'] = 'Judías del Ganxet 1/2 Ración';
+
+$translate['MONGETES'] = 'Judías';
 $translate['Naranja-Manzana FRUTA'] = 'Naranja-Manzana FRUTA';
 $translate['Nata amb Nous'] = 'Nata con nueces';
+$translate['NEGRA A LA BRASA - BUTIF'] = 'Butifarra negra a la brasa';
+
+$translate['PA TORRAT AMB TOMAQUET'] = 'Pan tostado con tomate';
+
+$translate['PA TORRAT'] = 'Pan tostado';
+$translate['PARRILLADA DE VERDURES A '] = 'Parrillada de verduras';
+$translate['PARRILLADA DE VERDURES A LA BRASA'] = 'parrillada de verduras a la brasa';
 $translate['Pasís Massini Individual'] = 'Pastel Massini Individual';
 $translate['Pastís Celebració (10-12 racions)'] = 'Pastel Celebración (10-12 raciones)';
 $translate['PASTIS CELEBRACIÓ'] = 'Pastel celebración';
 $translate['Pastis de Frmatge Casolà'] = 'Pastel de Queso Casero';
 $translate['Pastís Sacher'] = 'Tarta Sacher';
+$translate['Patates al Caliu 1/2 ració'] = 'Patatas al Caliu 1/2 ración';
+$translate['Patates al Caliu'] = 'Patatas en el Caliu';
+$translate['Patates Fregides 1/2 Ració'] = 'Patatas Fritas 1/2 Ración';
+
+$translate['PATATES FREGIDES'] = 'Patatas fritas';
+$translate['Pernil Ibèric d´Gla 1/2 Ració'] = '-Jamón Ibérico de Bellota 1/2 Ración';
+$translate['Pernil Ibèric d´glá'] = 'Jamón Ibérico de bellota';
+$translate['Pernil Serrà GR (duroc) 1/2 Ració'] = 'Jamón Serrano GR (duro) 1/2 Ración';
+
+$translate['PERNIL SERRÁ'] = 'Jamón serrano';
+$translate['PESCADO'] = 'Pescado';
+$translate['PESCADOS'] = 'PESCADOS';
+
+$translate['PEUS DE PORC A LA BRASA'] = 'Pies de cerdo a la brasa';
 $translate['Pijama Casolà'] = 'Pijama casero';
-$translate['Pinya Almibar'] = 'Piña Almíbar';
+$translate['PIÑA CON JAMON SERRANO'] = 'Piña con jamon serrano';
+$translate['PINNA FIDELIS RIVERA DUERO CRIANZA'] = 'Pinna fieles ribera Duero crianza';
 $translate['Pinya Almibar amb Nata'] = 'Piña Almíbar con Nata';
+$translate['Pinya Almibar'] = 'Piña Almíbar';
+$translate['PINYA AMB PERNIL'] = 'Piña con jamón';
 $translate['Pinya Fruita Natural'] = 'Piña Fruta Natural';
+
+$translate['POLLASTRE A LA BRASA'] = 'Pollo a la brasa (1/2 pieza)';
+$translate['POSTRES HELADOS'] = 'Postres helados';
+$translate['POSTRES'] = 'POSTRES';
 $translate['Precec Almibar'] = 'Melocotón Almíbar';
 $translate['Presec Almibar amb Nata'] = 'Melocotón Almíbar con Nata';
 $translate['Ració de Nata'] = 'Ración de Nata';
 $translate['racion de FRESAS'] = 'ración de fresas';
+
+$translate['RACIONES'] = 'RACIONES';
+$translate['Raimat Abadia Cabernet Sauvignon Negre'] = 'Raimat Abadía Cabernet Sauvignon Negro';
+$translate['ramon bilbao 1/2 pequeño'] = 'ramon bilbao 1/2 pequeño';
+$translate['RIOJA MARQUES DE CACERES CRIANZA'] = 'Rioja marqués de Cáceres, crianza';
+$translate['RIOJA MARQUES DE MURRIETA'] = 'marqués de Murrieta - Rioja';
+$translate['RIOJA MARQUES DE RISCAL RESERVA'] = 'Rioja marqués de riscal reserva';
+$translate['Roca Blanca (Montsant negre) Crianza'] = 'Roca Blanca (Montsant negre) Crianza';
+$translate['Secret de Porc a la brasa ibèric (250gr)'] = 'Secreto de Cerdo a la brasa ibérico (250gr)';
+$translate['SECRETO DE CERDO A LA BRASA'] = 'Secreto de cerdo a la brasa';
+
+$translate['SOPA DE LA CASA'] = 'Sopa de la casa';
+$translate['SORBETE LIMON C/MARC CAVA'] = 'Sorbete de limon c/Marc cava';
+$translate['SORBETE LIMON'] = 'Sorbete de limon';
+
+$translate['SURTIT DE PATES'] = 'Surtido de patés';
+$translate['TARTA AL WHISQUI C/NATA'] = 'Tarta al Whisqui c/nata';
+$translate['TARTA AL WHISQUI'] = 'Tarta al Whisqui';
+$translate['TORRADA AMB ESCALIVADA I '] = 'Tostada con escalivada y anchoas';
+$translate['TORRADA AMB ESCALIVADA I ANXOVES'] = 'Tostada con escalivada, anchoas' y atún;
+$translate['TRUFAS (6 unidades) CON NATA'] = 'Trufas (6 unidades) con nata';
+$translate['TRUFAS (6 unidades)'] = 'Trufas (6 unidades)';
 $translate['Trufas amb Nata 5 unitats'] = 'Trufas con Nata 5 unidades';
 $translate['Trufes Gelades (6 unitats)'] = 'Trufas Heladas (6 unidades)';
-$translate['POSTRES HELADOS'] = 'Postres helados';
-$translate['ALMENDRADO'] = 'Almendrado';
-$translate['COCO HELADO'] = 'Coco helado';
-$translate['CORNETTO VAINILLA Y CHOC'] = 'Corneto vainilla y chocolate';
-$translate['CROCANTI'] = 'Crocanti';
-$translate['CROCANTI CON COINTREAU'] = 'Crocanti con cointreau';
-$translate['CROCANTI CON NATA'] = 'Crocanti con nata';
-$translate['HELADO DE VAINILLA'] = 'Helado de vainilla';
-$translate['LIMON HELADO'] = 'Limon helado';
-$translate['SORBETE LIMON'] = 'Sorbete de limon';
-$translate['SORBETE LIMON C/MARC CAVA'] = 'Sorbete de limon c/Marc cava';
-$translate['TARTA AL WHISQUI'] = 'Tarta al Whisqui';
-$translate['TARTA AL WHISQUI C/NATA'] = 'Tarta al Whisqui c/nata';
-$translate['TRUFAS (6 unidades)'] = 'Trufas (6 unidades)';
-$translate['TRUFAS (6 unidades) CON NATA'] = 'Trufas (6 unidades) con nata';
+$translate['TST'] = 'TST_T';
 $translate['VALENCIANO'] = 'Valenciano';
+
+$translate['VEDELLA AMB VOLETS'] = 'Ternera con setas';
+$translate['XAI A LA BRASA (3 PECES)'] = 'Cordero a la brasa (3 piezas)';
+
+$translate['Xatonada casolana'] = 'Xatonada casera';
+$translate['Xoriço Cular 1/2 Ració'] = 'Chorizo ​​Cular 1/2 Ración';
+
+$translate['XORIÇO'] = 'Chorizo';
+$translate['YLLERA - CASTELLA I LLEÓ-'] = 'Yllera - castilla y león';
  
-// Check for duplicated indexes
-$duplicate_indexes = array_keys(array_filter(array_count_values(array_keys($translate)), function($count) {
-    return $count > 1;
-}));
-
-echo json_encode($duplicate_indexes);
-die();
-
 require_once('translate.php');
 ?>
