1cb3b3811891ea3d12d143cdb59532d990bb0d59
alexinfopruna <alexinfopruna@gmail.com>
2025-02-17 12:54:37 +0100

    222

--- cb-reserves/taules/gestor_reserves.php
+++ cb-reserves/taules/gestor_reserves.php
@@ -3119,9 +3119,11 @@ ORDER BY `estat_hores_data` DESC";
       $result = $service->send($message);
 
       $headerService = new \Esendex\MessageHeaderService($authentication);
-      $message = $headerService->message("messageId");
-      echo "Message ID: " . $message->id() . "\n";
-      print_r($message->status());
+      echo "RESULT: ";
+      print_r($result);
+      //$message = $headerService->message($result->id);
+      //echo "Message ID: " . $message->id() . "\n";
+      //print_r($message->status());
       //echo "ESENDEX-----ENVIANT: $num - $msg";
 
     } catch (Exception $e) {
