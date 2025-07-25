b495795037ae76f5fa2752466be574ebc2567aea
alexinfopruna <alexinfopruna@gmail.com>
2025-07-25 12:00:07 +0200

    SENSE header json utf8 --> fem text/html + json parse

--- cb-reserves/taules/Gestor.php
+++ cb-reserves/taules/Gestor.php
@@ -857,7 +857,7 @@ class Gestor {
 
     $json = json_encode($resposta);
     $json = Gestor::remove_bom($json);
-    header('Content-Type: application/json; charset=utf-8');
+    //header('Content-Type: application/json; charset=utf-8');
     return $json;
   }
 
