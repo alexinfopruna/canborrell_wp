706b7af39ff2901623e78c266782cc2c694d977d
alexinfopruna <alexinfopruna@gmail.com>
2026-04-27 10:49:09 +0200

    opcio terrassa

--- wp-content/themes/onetone_cb/page-reservar.php
+++ wp-content/themes/onetone_cb/page-reservar.php
@@ -829,7 +829,8 @@ if ($testTPV) {
                                                                     <?php
                                                                             
                                                                             $opcio_terrassa = OPCIO_TERRASSA ;
-                                                                          ?>
+                                                                            $hidden = $opcio_terrassa > 0 ? '' : 'style="display:none;"'; ?>
+                                                                        <div id="opcio_terrassa" class="s" <?php echo $hidden ?>>
                                                                          <div id='opcio_terrassa' style="<?php echo $opcio_terrassa>0?"":"display:none"?>">
                                                                           <h4 id="titol_SelectorCadiraRodes"><?php l('Vols que la taula sigui a la terrassa?'); ?>:</h4>
                                                                               <div class="ui-corner-all caixa caixa100 dere hores info_hora">
